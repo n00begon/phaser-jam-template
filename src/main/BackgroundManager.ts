44d7c5e57a6b05973a685e2e50046da9fb7d04cb
Tim Neumegen <tneumegen@gmail.com>
2020-10-25 00:16:58 +1300

    Changed to tile sprites

--- src/main/BackgroundManager.ts
+++ src/main/BackgroundManager.ts
@@ -6,13 +6,22 @@ export class BackgroundManager {
      * Adds the parallax background to the scene
      */
     constructor(scene: Phaser.Scene) {
-        const background = scene.add.image(scene.sys.canvas.width / 2, 300, "background", "backgroundHill");
-        background.setDepth(-1); // Depth -1 to ensure it is behind the gameplay
-        background.setScrollFactor(0.2); // A scroll factor lower than one means it moves slower as the camera moves giving it a distant look
+        // const background = scene.add.tileSprite(
+        //     scene.sys.canvas.width / 2,
+        //     300,
+        //     10000,
+        //     2000,
+        //     "background",
+        //     "backgroundHill",
+        // );
+        // background.setDepth(-1); // Depth -1 to ensure it is behind the gameplay
+        // background.setScrollFactor(0.2); // A scroll factor lower than one means it moves slower as the camera moves giving it a distant look
 
-        const sky = scene.add.image(
-            scene.sys.canvas.width / 2,
+        const sky = scene.add.tileSprite(
+            0,
             (scene.sys.canvas.height / 8) * 5,
+            10000,
+            5000,
             "background",
             "backgroundEmpty",
         );

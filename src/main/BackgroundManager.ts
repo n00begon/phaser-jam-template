e08603c3e3ef0ea356053129774339d83c43d018
Tim Neumegen <tneumegen@gmail.com>
2020-10-25 23:32:39 +1300

    Changed to repeating background tiles.

--- src/main/BackgroundManager.ts
+++ src/main/BackgroundManager.ts
@@ -6,25 +6,19 @@ export class BackgroundManager {
      * Adds the parallax background to the scene
      */
     constructor(scene: Phaser.Scene) {
-        // const background = scene.add.tileSprite(
-        //     scene.sys.canvas.width / 2,
-        //     300,
-        //     10000,
-        //     2000,
-        //     "background",
-        //     "backgroundHill",
-        // );
-        // background.setDepth(-1); // Depth -1 to ensure it is behind the gameplay
-        // background.setScrollFactor(0.2); // A scroll factor lower than one means it moves slower as the camera moves giving it a distant look
-
-        const sky = scene.add.tileSprite(
-            0,
-            (scene.sys.canvas.height / 8) * 5,
+        const background = scene.add.tileSprite(
+            scene.sys.canvas.width / 2,
+            700,
             10000,
-            5000,
+            780,
             "background",
-            "backgroundEmpty",
+            "backgroundHill",
         );
+        background.setTilePosition(0, 1); // Removes the one pixel line that shows when the camera moves.
+        background.setDepth(-1); // Depth -1 to ensure it is behind the gameplay
+        background.setScrollFactor(0.2); // A scroll factor lower than one means it moves slower as the camera moves giving it a distant look
+
+        const sky = scene.add.tileSprite(0, (scene.sys.canvas.height / 8) * 4, 4096, 1024, "background", "sky");
         sky.setDepth(-2);
         sky.setScrollFactor(0.05);
         sky.setScale(1.5);
